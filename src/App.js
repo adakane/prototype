ca4008b0a3f244c7e911fbfbcea6af9fc7d60559
adakane <67022596+adakane@users.noreply.github.com>
2021-06-27 15:24:38 -0400

    navbar and login
    i just need to create navlink and have the sign in tab work

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Example from "./components/Navbar/Navbar";
+import SignIn from "./components/Authentication/Signin";
+
+function App() {
+  return (
+    <div className="App">
+      <Example />
+    </div>
+  );
+}
+
+export default App;
